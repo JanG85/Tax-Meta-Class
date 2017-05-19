8972e50baa16f6ee3827b11354bdf42ca766342a
JanG85 <JanG85@users.noreply.github.com>
2017-05-19 13:03:32 +0200

    fix wpColorPicker error
    Err: Uncaught TypeError: $(...).wpColorPicker is not a function
    
    adding jquery and WP-COLOR-PICKER as dependencies at line 162 fixes this issue. Image upload is working also fine again.
    
    Great Plugin!
    
    Best regards!

--- Tax-meta-class/Tax-meta-class.php
+++ Tax-meta-class/Tax-meta-class.php
@@ -159,7 +159,7 @@ class Tax_Meta_Class {
       // Enqueue Meta Box Style
       wp_enqueue_style( 'tax-meta-clss', $plugin_path . '/css/Tax-meta-class.css' );
       // Enqueue Meta Box Scripts
-      wp_enqueue_script( 'tax-meta-clss', $plugin_path . '/js/tax-meta-clss.js', array( 'jquery' ), null, true );
+      wp_enqueue_script( 'tax-meta-clss', $plugin_path . '/js/tax-meta-clss.js', array( 'jquery', 'wp-color-picker' ), null, true );
     
     }
     
